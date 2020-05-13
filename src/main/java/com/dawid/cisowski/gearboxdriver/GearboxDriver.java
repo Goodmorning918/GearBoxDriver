9f04bc037bc2a62eb97d7089809e4ca04880d4ee
Dawid.Cisowski <d.cisowski@consultant.crif.com>
2020-05-13 19:20:25 +0200

    [DaCi] add external api

---
+++ src/main/java/com/dawid/cisowski/gearboxdriver/GearboxDriver.java
@@ -0,0 +1,13 @@
+package com.dawid.cisowski.gearboxdriver;
+
+/**
+ * GearboxDriver
+ * Created by Dawid Cisowski
+ * on 13.05.20.
+ */
+
+public class GearboxDriver {
+
+  public static void main(String[] args) {
+  }
+}
